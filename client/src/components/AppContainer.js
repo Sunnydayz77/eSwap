2c333905df982b7d56f0af1b1c3306f560718192
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-03-21 16:27:40 -0400

    commiting changes before heroku/atlas deployement

--- client/src/components/AppContainer.js
+++ client/src/components/AppContainer.js
@@ -7,16 +7,10 @@ import SignUp from '../screens/SignUp';
 // import Navbar from '../layouts/Navbar';
 import Header from '../screens/Header';
 import SignIn from '../screens/SignIn';
-<<<<<<< HEAD
-// import { Route } from 'react-router-dom';
-
-export class AppContainer extends Component {
-=======
 import { verifyToken } from '../services/auth'
 import { verifyUser } from '../services/auth'
 
 class AppContainer extends Component {
->>>>>>> afe18ac3f871fb5e50d03234a062054c66694b5f
   constructor() {
     super();
     this.state = {
@@ -25,31 +19,17 @@ class AppContainer extends Component {
     }
   }
 
-<<<<<<< HEAD
-  async componentDidMount() {
-    try {
-      const items = await getItems()
-      this.setState({ items })
-    } catch (err) {
-      console.error(err)
-=======
   async componentDidMount() { 
     const user = await verifyToken();
     if (user) {
       this.setState({ user })
       const items = await getItems()
       this.setState({ items })
->>>>>>> afe18ac3f871fb5e50d03234a062054c66694b5f
     }
   }
 
   addItem = item => this.setState({ items: [...this.state.items, item] })
 
-<<<<<<< HEAD
-  setUser = user => this.setState({ user })
-
-  clearUser = () => this.setState({ user: null })
-=======
   deleteItemFromList = itemId => this.setState(prevState => ({
     items: prevState.items.filter(item =>
       item._id !== itemId
@@ -62,7 +42,6 @@ class AppContainer extends Component {
   }
 
   clearUser = () => this.setState({ user: null, items: [] })
->>>>>>> afe18ac3f871fb5e50d03234a062054c66694b5f
 
   render() {
     const { user, items } = this.state
@@ -77,10 +56,7 @@ class AppContainer extends Component {
             setUser={this.setUser}
             addItem={this.addItem}
             clearUser={this.clearUser}
-<<<<<<< HEAD
-=======
             deleteItemFromList={this.deleteItemFromList}
->>>>>>> afe18ac3f871fb5e50d03234a062054c66694b5f
           />
         </Switch>
       </div>
